3e56b6c4e412dc7c6ce64f32690739d7ae226cfa
Sinan Karakaya <sinankarakaya1992@gmail.com>
2022-02-01 10:05:00 +0300

    01.02.2022

---
+++ authserver/src/main/java/org/authserver/App.java
@@ -0,0 +1,13 @@
+package org.authserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
